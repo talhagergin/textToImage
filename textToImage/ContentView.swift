1092b483a99af446eaf68311d2ebdd20d2f2d348
Talha Gergin <talhagergin6@gmail.com>
2025-01-09 01:56:23 +0300

    Initial Commit

---
+++ textToImage/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  textToImage
+//
+//  Created by Talha Gergin on 9.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ textToImage/textToImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  textToImageApp.swift
+//  textToImage
+//
+//  Created by Talha Gergin on 9.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct textToImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
